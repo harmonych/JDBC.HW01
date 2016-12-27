34a6b22b7c6339ae16d4f54e6cfdc1fa4db6ec3d
Harmony <duke.harmony@gmail.com>
2016-12-27 12:46:23 +0800

    no message

--- hw01/PetDeleteMain.java
+++ hw01/PetDeleteMain.java
@@ -1,12 +1,13 @@
 package hw01;
 
-import hw01.util.DBUtils;
-
 public class PetDeleteMain {
 
 	public static void main(String[] args) {		
 		PetDAO dao = new PetDAO();
-		dao.delete(4);  //此處輸入要刪除該組資料的id
+		int n = dao.delete(4);  //此處輸入要刪除該組資料的id
+		if (n==0){
+			System.out.println("未有該輸入id組別的資料。");
+		}
 	}
 
 }
